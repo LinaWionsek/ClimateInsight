82bd09198381d2a51d33697c05e013ab3815b25c
fshippman <fshippman@yahoo.com>
2024-10-02 13:25:20 +0200

    created input field to submit city name to show infos in console

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,7 +9,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
  ApiKey = '037abbf0f7839788885661e47e98c7e3'
-  getData(): Observable<any> {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=london&appid=037abbf0f7839788885661e47e98c7e3');
+  getData(city: string): Observable<any> {
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=037abbf0f7839788885661e47e98c7e3');
   }
 }
--- src/app/main/weather-card/weather-card.component.ts
+++ src/app/main/weather-card/weather-card.component.ts
@@ -1,23 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from '../../data.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-weather-card',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './weather-card.component.html',
-  styleUrl: './weather-card.component.scss'
+  styleUrl: './weather-card.component.scss',
 })
 export class WeatherCardComponent {
- 
   private apiService = inject(DataService);
   data: any;
-
+  temp: any;
+  name: any;
+  cityName: string = "";
   ngOnInit() {
-    this.apiService.getData().subscribe((response) => {
+  //  this.firstLoad()
+  }
+  dialogFunction() {
+    console.log(this.cityName);
+    this.firstLoad()
+  }
+
+  firstLoad() {
+    this.apiService.getData(this.cityName).subscribe((response) => {
       this.data = response;
       console.log(this.data);
+      // this.temp = this.data.main.temp;
+      // this.name = this.data.name;
     });
   }
 
+  
 }
